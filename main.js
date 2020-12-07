636a428a817af9b589ae3d60f66c4d78a1010f4d
udaibirv <udaibirv@gmail.com>
2020-12-07 13:12:16 -0800

    finished up feature #1

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 var $addEntryButton = document.getElementById('add-entry-button');
 var $modal = document.querySelector('.modal');
 var $body = document.querySelector('body');
+var $submitModalButton = document.getElementById("submit-modal-button")
 
 $addEntryButton.addEventListener('click', function (event) {
   $modal.className = 'modal';
   $body.className = 'overlay';
 });
+
+$submitModalButton.addEventListener('click', function(event){
+  $modal.className = 'hidden';
+  $body.className = '';
+})
