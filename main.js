1acf88bd9a3b2db90f83e0c0220e1c35eee5de17
Josh Tjiauw <freezingmist2@gmail.com>
2020-12-07 15:33:52 -0800

    finished table rows and head

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 var $addEntryButton = document.getElementById('add-entry-button');
 var $modal = document.querySelector('.modal');
 var $body = document.querySelector('body');
-var $submitModalButton = document.getElementById("submit-modal-button")
+var $submitModalButton = document.getElementById("submit-modal-button");
+var $sunday = document.getElementById("button-sunday");
+var $monday = document.getElementById("button-monday");
+var $tuesday = document.getElementById("button-tuesday");
+var $wednesday = document.getElementById("button-wednesday");
+var $thursday = document.getElementById("button-thursday");
+var $friday = document.getElementById("button-friday");
+var $saturday = document.getElementById("button-saturday");
 
 $addEntryButton.addEventListener('click', function (event) {
   $modal.className = 'modal';
@@ -12,3 +19,8 @@ $submitModalButton.addEventListener('click', function(event){
   $modal.className = 'hidden';
   $body.className = '';
 })
+
+$sunday.addEventListener('click', function(event){
+
+})
+document.addEventListener()
